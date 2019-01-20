68ce8c38b2836eb2b505d68cffbfe1db95dee35b
Joey2Jobs <scholz.joe@gmail.com>
2019-01-20 13:34:33 -0600

    HTML and Firebase

---
+++ assets/js/logic.js
@@ -0,0 +1,13 @@
+var config = {
+    apiKey: "AIzaSyD6BYUcH9Teccw6SwJrk_sr5r9dSxoTOyU",
+    authDomain: "trainscheduler-5c713.firebaseapp.com",
+    databaseURL: "https://trainscheduler-5c713.firebaseio.com",
+    projectId: "trainscheduler-5c713",
+    storageBucket: "trainscheduler-5c713.appspot.com",
+    messagingSenderId: "194759685154"
+  };
+  firebase.initializeApp(config);
+
+//   database variable for ease of reference
+
+  var database = firebase.database();
